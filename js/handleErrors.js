2707303774ff749e3263bf8b47bff0d3e16e02a2
VuongDB <dobavuong_t62@hus.edu.vn>
2023-05-17 14:53:18 +0700

    update FE

---
+++ js/handleErrors.js
@@ -0,0 +1,31 @@
+// Hàm xử lý lỗi chung
+export function handleErrors(error) {
+    if (error.response) {
+      switch (error.response.status) {
+        case 400:
+          console.error('Lỗi 400 - Bad Request:', error);
+          // Xử lý lỗi 400
+          break;
+        case 401:
+          console.error('Lỗi 401 - Unauthorized:', error);
+          // Xử lý lỗi 401
+          break;
+        case 403:
+          console.log('Lỗi 403 - Forbidden');
+          window.location.href = '../exception/403.html'; // Chuyển hướng đến trang 403.html
+          break;
+        case 404:
+          console.error('Lỗi 404 - Not Found:', error);
+          // Xử lý lỗi 404
+          break;
+        default:
+          console.error('Lỗi không xác định:', error);
+          // Xử lý lỗi không xác định
+          break;
+      }
+    } else {
+      console.error('Đăng nhập thất bại:', error);
+    }
+  }
+  
+  
--- js/login.js
+++ js/login.js
@@ -1,26 +1,28 @@
+import { handleErrors } from './handleErrors.js';
 document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.getElementById('loginForm');
-    loginForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-  
-      axios.post('http://localhost:8082/api/v1/auth/authenticate', {
+  const loginButton = document.getElementById('loginButton');
+  loginButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('typeEmailX-2').value;
+    const password = document.getElementById('typePasswordX-2').value;
+
+    const apiUrl = 'http://localhost:8082/api/v1/auth/authenticate';
+
+    axios
+      .post(apiUrl, {
         email: email,
         password: password
       })
-        .then(response => {
-            console.log("========================")
-            console.log(response)
-          const data = response.data;
-          localStorage.setItem('accessToken', data.access_token);
-          localStorage.setItem('refreshToken', data.refresh_token);
-          console.log('Đăng nhập thành công');
-          window.location.href = 'index.html';
-        })
-        .catch(error => {
-          console.error('Đăng nhập thất bại:', error);
-        });
-    });
+      .then(response => {
+        const data = response.data;
+        localStorage.setItem('accessToken', data.access_token);
+        localStorage.setItem('refreshToken', data.refresh_token);
+        console.log('Đăng nhập thành công');
+        window.location.href = 'index.html'; // Chuyển hướng sau khi đăng nhập thành công
+      })
+      .catch(error => {
+        handleErrors(error);
+      });
   });
+});
